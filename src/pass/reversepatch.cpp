315ce37bd799a773e5bfd8bb84b6002d351e71c0
shinmao <shinmaox2016@gmail.com>
2021-09-29 23:40:48 -0400

    update reversepatch for API

--- src/pass/reversepatch.cpp
+++ src/pass/reversepatch.cpp
@@ -6,49 +6,59 @@
 #include "analysis/frametype.h"
 
 void ReversePatch::visit(Function *function) {
-    std::cout << "[function] visiting...\n";
-	if (function->getName() == "main") {
-        std::cout << "[function] visiting main...\n";
-	    auto block3 = function->getChildren()->getIterable()->get(2);
-	    auto block4 = function->getChildren()->getIterable()->get(3);
-	    auto block5 = function->getChildren()->getIterable()->get(4);
-	    
-        ChunkMutator mutator(function);
-	    mutator.remove(block3);
-	    mutator.remove(block4);
-        mutator.remove(block5);
+  if (function->getName() == "main") {
+    auto block3 = function->getChildren()->getIterable()->get(2);
+    auto block4 = function->getChildren()->getIterable()->get(3);
+    auto block5 = function->getChildren()->getIterable()->get(4);
+    target_func.push_back("main");
+    rmBlk.push_back(block3);
+    target_func.push_back("main");
+    rmBlk.push_back(block4);
+    target_func.push_back("main");
+    rmBlk.push_back(block5);
+  }
+
+  std::cout << "[function] visiting...\n";
+  for (int i = 0; i < target_func.size(); i++) {
+    if(function->getName() == target_func[i]) {
+      std::cout << "[function] visiting " << function->getName() << "...\n";
+      ChunkMutator mutator(function);
+      if (rmBlk.size() > i) {
+        mutator.remove(rmBlk[i]);
+      }
+      if (insbeforeBlk.size() > i) {
+        mutator.insertBefore(insbeforeB[i], insbeforeBlk[i]);
+      }
     }
-    else {
-        std::cout << "[function] main function not found...\n";
-    	assert("You need main function in your ELF!\n");
-    }
-    recurse(function);
+  }
+  recurse(function);
 }
 
 void ReversePatch::visit(Block *block) {
+  /**
     std::cout << "[block] visiting block of main function...\n";
     if (block->getName() == "main/bb+0") {
-        std::cout << "[block] visiting first block of main function...\n";
-        auto parent = dynamic_cast<Function *>(block->getParent());
-        FrameType frameType(parent);
-        auto prologueEnd = frameType.getSetSPInstr();
+      std::cout << "[block] visiting first block of main function...\n";
+      auto parent = dynamic_cast<Function *>(block->getParent());
+      FrameType frameType(parent);
+      auto prologueEnd = frameType.getSetSPInstr();
 
-        // change stack size from 0x30 to 0x20
-        ChunkMutator mutator(block);
-        mutator.insertBefore(prologueEnd, Disassemble::instruction({0x48, 0x83, 0xec, 0x20}));
-        // remove original prologue end
-        mutator.remove(prologueEnd);
+      // change stack size from 0x30 to 0x20
+      ChunkMutator mutator(block);
+      mutator.insertBefore(prologueEnd, Disassemble::instruction({0x48, 0x83, 0xec, 0x20}));
+      // remove original prologue end
+      mutator.remove(prologueEnd);
     }
     else if (block->getName() == "main/bb+35") {
-        std::cout << "[block] visiting second block of main function...\n";
-        auto instr3 = block->getChildren()->getIterable()->get(2);
-        ChunkMutator mutator(block);
-        mutator.insertBefore(instr3, Disassemble::instruction({0x48, 0x8d, 0x45, 0xe0}));
-        // remove original
-        mutator.remove(instr3);
+      std::cout << "[block] visiting second block of main function...\n";
+      auto instr3 = block->getChildren()->getIterable()->get(2);
+      ChunkMutator mutator(block);
+      mutator.insertBefore(instr3, Disassemble::instruction({0x48, 0x8d, 0x45, 0xe0}));
+      // remove original
+      mutator.remove(instr3);
     }
     else {
-        std::cout << "[block] not first and second block of main function...\n";
-    }
+      std::cout << "[block] not first and second block of main function...\n";
+    }**/
     //recurse(block);
 }
--- src/pass/reversepatch.h
+++ src/pass/reversepatch.h
@@ -4,10 +4,21 @@
 #include "chunkpass.h"
 
 class ReversePatch : public ChunkPass {
+private:
+  std::vector<Function*> rmFunc;
+  std::vector<Function*> appendFunc;
+  std::vector<std::string> target_func;
+  std::vector<Block*> rmBlk;
+  std::vector<Block*> insbeforeB;
+  std::vector<Block*> insbeforeBlk;
+  std::vector<std::string> target_block;
+  std::vector<Instruction*> rmIns;
+  std::vector<Instruction*> insbeforeI;
+  std::vector<Instruction*> insbeforeIns;
 public:
-    ReversePatch() {}
-    virtual void visit(Function *function);
-    virtual void visit(Block *block);
+  ReversePatch(){}
+  virtual void visit(Function *function);
+  virtual void visit(Block *block);
 };
 
 #endif
