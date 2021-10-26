51f3abcab2b953d0b682c95ed92fe928ef2151b6
shinmao <shinmaox2016@gmail.com>
2021-10-26 16:43:41 -0400

    [revpatch] build up pass to identify func signatures of two modules

--- src/pass/reversepatch.cpp
+++ src/pass/reversepatch.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+// #include <openssl>
 #include "reversepatch.h"
 #include "disasm/disassemble.h"
 #include "instr/linked-x86_64.h"
@@ -9,12 +10,30 @@
 // current goal
 // to get the signature of function
 
+void ReversePatch::visit(Module *module) {
+  auto program = static_cast<Program *>(module->getParent());
+  std::cout << "Compare module-[" << module->getName() << "] with module-[" << comparedModule->getName()
+    << "]\n";
+  recurse(module);
+  for(auto sig : fsign) { elfsign.push_back(sig); }
+  std::cout << "For elfsign: " << elfsign[0] << "\n";
+  fsign.clear();
+  recurse(comparedModule);
+  for(auto sig : fsign) { cmpelfsign.push_back(sig); }
+  std::cout << "For cmpelfsign: " << cmpelfsign[0] << "\n";
+  fsign.clear();
+}
+
+void ReversePatch::visit(FunctionList *functionlist) {
+  recurse(functionlist);
+  std::cout << "function list: " << fsign.size() << "\n";
+}
+
 void ReversePatch::visit(Function *function) {
-  std::cout << "function name: " << function->getName() << "\n";
-  Signature funcSign;
-  funcSign.fsignature = "";
   recurse(function);
-  std::cout << "fsign: " << funcSign.fsignature << "\n";
+  std::cout << "function name: " << function->getName() << " with sign: " << sign << "\n";
+  fsign.push_back(sign);
+  sign = "";
 }
 
 void ReversePatch::visit(Block *block) {
@@ -23,59 +42,40 @@ void ReversePatch::visit(Block *block) {
 }
 
 void ReversePatch::visit(Instruction *instruction) {
-  InstructionSign instrsign;
-  instruction->getSemantic()->accept(&instrsign);
-}
-
-void InstructionSign::visit(IsolatedInstruction *semantic) {
-  // std::cout << "Isolated Instruction: ";
-  //funcSign.fsignature += semantic->getAssembly()->getMnemonic();
-  //std::cout << funcSign.fsignature << "\n";
-}
-
-void InstructionSign::visit(LinkedInstruction *semantic) {
-  //std::cout << "Linked Instruction: ";
-  //funcSign.fsignature += semantic->getAssembly()->getMnemonic();
-}
-
-void InstructionSign::visit(ControlFlowInstruction *semantic) {
-  //std::cout << "ControlFlow Instruction: ";
-  //funcSign.fsignature += semantic->getMnemonic();
-}
-
-#ifdef ARCH_X86_64
-void InstructionSign::visit(DataLinkedControlFlowInstruction *semantic) {
-  //std::cout << "Data Linked ControlFlow Instruction: ";
-  //funcSign.fsignature += semantic->getAssembly()->getMnemonic();
-}
-#endif
-
-void InstructionSign::visit(ReturnInstruction *semantic) {
-  //std::cout << "Ret: ";
-  //funcSign.fsignature += semantic->getAssembly()->getMnemonic();
-}
-
-void InstructionSign::visit(IndirectJumpInstruction *semantic) {
-  //std::cout << "Indirect Jump Instruction: ";
-  //funcSign.fsignature += semantic->getMnemonic();
-}
-
-void InstructionSign::visit(IndirectCallInstruction *semantic) {
-  //std::cout << "Indirect call Instruction: ";
-  //funcSign.fsignature += "CALL";
-}
-
-void InstructionSign::visit(StackFrameInstruction *semantic) {
-  //std::cout << "stack frame Instruction: ";
-  //funcSign.fsignature += semantic->getAssembly()->getMnemonic();
-}
-
-void InstructionSign::visit(LiteralInstruction *semantic) {
-  //std::cout << "Literal Instruction: ";
-  //funcSign.fsignature += semantic->getAssembly()->getMnemonic();
-}
-
-void InstructionSign::visit(LinkedLiteralInstruction *semantic) {
-  //std::cout << "Linked literal Instruction: ";
-  //funcSign.fsignature += semantic->getAssembly()->getMnemonic();
+  // InstructionSign inssign;
+  // instruction->getSemantic()->accept(&inssign);
+  auto semantic = instruction->getSemantic();
+  #ifdef ARCH_X86_64
+  if(dynamic_cast<DataLinkedControlFlowInstruction *>(semantic)) {
+    sign += dynamic_cast<DataLinkedControlFlowInstruction *>(semantic)->getAssembly()->getMnemonic();
+    return;
+  }
+  #endif
+  if(dynamic_cast<IsolatedInstruction *>(semantic)) {
+    sign += dynamic_cast<IsolatedInstruction *>(semantic)->getAssembly()->getMnemonic();
+  }
+  else if (dynamic_cast<LinkedInstruction *>(semantic)) {
+    sign += dynamic_cast<LinkedInstruction *>(semantic)->getAssembly()->getMnemonic();
+  }
+  else if (dynamic_cast<ReturnInstruction *>(semantic)) {
+    sign += dynamic_cast<ReturnInstruction *>(semantic)->getAssembly()->getMnemonic();
+  }
+  else if (dynamic_cast<IndirectCallInstruction *>(semantic)) {
+    sign += dynamic_cast<IndirectCallInstruction *>(semantic)->getAssembly()->getMnemonic();
+  }
+  else if (dynamic_cast<StackFrameInstruction *>(semantic)) {
+    sign += dynamic_cast<StackFrameInstruction *>(semantic)->getAssembly()->getMnemonic();
+  }
+  else if(dynamic_cast<LiteralInstruction *>(semantic)) {
+    sign += dynamic_cast<LiteralInstruction *>(semantic)->getAssembly()->getMnemonic();
+  }
+  else if(dynamic_cast<LinkedLiteralInstruction *>(semantic)) {
+    sign += dynamic_cast<LinkedLiteralInstruction *>(semantic)->getAssembly()->getMnemonic();
+  }
+  else if(dynamic_cast<ControlFlowInstruction *>(semantic)) {
+    sign += dynamic_cast<ControlFlowInstruction *>(semantic)->getMnemonic();
+  }
+  else if(dynamic_cast<IndirectJumpInstruction *>(semantic)) {
+    sign += dynamic_cast<IndirectJumpInstruction *>(semantic)->getMnemonic();
+  }
 }
--- src/pass/reversepatch.h
+++ src/pass/reversepatch.h
@@ -1,39 +1,25 @@
 #ifndef EGALITO_PASS_REVERSEPATCH_H
 #define EGALITO_PASS_REVERSEPATCH_H
 
+#include <vector>
 #include "chunkpass.h"
 #include "instr/visitor.h"
 #include "instr/concrete.h"
 
-class Signature {
-public:
-  std::string fsignature;
-};
-
 class ReversePatch : public ChunkPass {
+private:
+  Module *comparedModule;
+  std::vector<std::string> elfsign;
+  std::vector<std::string> cmpelfsign;
+  std::vector<std::string> fsign;
+  std::string sign;
 public:
-  std::string fsign;
+  ReversePatch(Module *comparedModule) : comparedModule(comparedModule) {}
   virtual ~ReversePatch() { std::cout << "Revdone\n"; }
+  virtual void visit(Module *module);
+  virtual void visit(FunctionList *functionlist);
   virtual void visit(Function *function);
   virtual void visit(Block *block);
   virtual void visit(Instruction *instruction);
 };
-
-class InstructionSign : public InstructionVisitor, public ReversePatch {
-public:
-  ~InstructionSign() { std::cout << "InstructionSign\n"; }
-  void visit(IsolatedInstruction *semantic);
-  void visit(LinkedInstruction *semantic);
-  void visit(ControlFlowInstruction *semantic);
-#ifdef ARCH_X86_64
-  void visit(DataLinkedControlFlowInstruction *semantic);
-#endif
-  void visit(ReturnInstruction *semantic);
-  void visit(IndirectJumpInstruction *semantic);
-  void visit(IndirectCallInstruction *semantic);
-  void visit(StackFrameInstruction *semantic);
-  void visit(LiteralInstruction *semantic);
-  void visit(LinkedLiteralInstruction *semantic);
-};
-
 #endif
